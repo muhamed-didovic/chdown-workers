ac23322d1ce2ab8e0a163c47507683413ed68df1
Muhamed Didovic <muhamed.didovic@gmail.com>
2022-06-20 00:08:25 +0200

    clean

--- src/helpers/writeWaitingInfo.js
+++ src/helpers/writeWaitingInfo.js
@@ -1,8 +1,24 @@
 'use strict';
 
 const cleanLine = require('./cleanLine');
-
-function writeWaitingInfo(state, materialsName, ms, random, { localSizeInBytes, remoteSizeInBytes }) {
+const formatBytes = (bytes, decimals) => {
+    if (bytes == 0) return '0 Bytes';
+    let k     = 1024,
+        dm    = decimals || 2,
+        sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
+        i     = Math.floor(Math.log(bytes)/Math.log(k));
+    return parseFloat((bytes/Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+};
+const secondsToHms = sec => {
+    const h = Math.floor(sec/3600);
+    const m = Math.floor(sec%3600/60);
+    const s = Math.floor(sec%3600%60);
+    const hh = h < 10 ? '0' + h : h;
+    const mm = m < 10 ? '0' + m : m;
+    const ss = s < 10 ? '0' + s : s;
+    return `${hh}:${mm}:${ss}`;
+};
+const writeWaitingInfo = (state, materialsName, ms, random, { localSizeInBytes, remoteSizeInBytes }) => {
     cleanLine();
     const percent     = (state.percent*100).toFixed(2),
           transferred = formatBytes(state.size.transferred),
@@ -12,29 +28,7 @@ function writeWaitingInfo(state, materialsName, ms, random, { localSizeInBytes, 
           t           = `Downloading: ${percent}% | ${transferred} / ${total} | ${speed}/sec | ${remaining} - ${materialsName.split('/').pop()} Found:${localSizeInBytes}/${remoteSizeInBytes}`;
     //process.stdout.write(text);
     ms.update(random, { text: t, color: 'blue' });
-}
-
-function formatBytes(bytes, decimals) {
-    if (bytes == 0) return '0 Bytes';
-    let k     = 1024,
-        dm    = decimals || 2,
-        sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
-        i     = Math.floor(Math.log(bytes)/Math.log(k));
-    return parseFloat((bytes/Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
-}
-
-function secondsToHms(sec) {
-    const h = Math.floor(sec/3600);
-    const m = Math.floor(sec%3600/60);
-    const s = Math.floor(sec%3600%60);
-    const hh = h < 10 ? '0' + h : h;
-    const mm = m < 10 ? '0' + m : m;
-    const ss = s < 10 ? '0' + s : s;
-    return `${hh}:${mm}:${ss}`;
-}
-
-// module.exports = writeWaitingInfo;
-// module.exports = formatBytes;
+};
 
 module.exports = {
     writeWaitingInfo,
